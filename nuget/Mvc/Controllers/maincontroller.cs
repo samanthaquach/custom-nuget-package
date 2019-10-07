a1236c1a8d012c1a587662d096c4e2be9a6fdcbd
Samantha Quach <samanthatquach@gmail.com>
2019-10-07 15:34:11 -0500

    first commit

---
+++ nuget/Mvc/Controllers/maincontroller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nuget.Mvc.Controllers
+{
+    public class maincontroller
+    {
+        private string Collection()
+        {
+            return String.Empty;
+        }
+    }
+}
